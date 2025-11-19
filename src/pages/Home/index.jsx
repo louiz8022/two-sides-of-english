69175b26d80da8bb6fa6a5cd8b4e06f74f102c87
Adriano Souza <adrianoads910@gmail.com>
2025-11-19 00:25:52 -0300

    theme and collor costumer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -24,8 +24,10 @@ export const Home = () => {
 
     return (
         <main>
-
-            <div className="bg-amber-50 space-y-10 text-slate-950 min-h-dvh flex flex-col items-center justify-center">
+            <div className="bg-brand-gray space-y-10 text-slate-950 min-h-dvh flex flex-col items-center justify-center">
+                <picture>      
+                    <img src="./logo.png" alt="two-side-of-english" className="w-36 h-56w-56 h-56  flex items-center justify-center "/>
+                </picture>
                 <article className="w-full text-center space-y-5 flex flex-col items-center justify-center">
                     <h2 className="text-2xl "> Interactive Language Quiz </h2>
                     <h1 className="font-black text-5xl font-momo">British vs American <br /> English</h1>
@@ -34,29 +36,29 @@ export const Home = () => {
 
                 <div className="grid lg:grid-cols-3 space-y-5 space-x-5">
                     <section className="space-y-4 w-max h-max">
-                        <figure className="w-36 h-36 rounded-full bg-pink-300 flex items-center justify-center ">
+                        <figure className="w-36 h-36 rounded-full bg-brand-red flex items-center justify-center ">
                             <BookOpen size="70px" color="white" />
                         </figure>
 
-                        <p className="text-center text-lg font-medium">10 Questions</p>
+                        <p className="text-center text-lg font-medium">30 Questions</p>
                     </section>
 
                     <section className="space-y-4 w-max h-max">
-                        <figure className="w-36 h-36 rounded-full bg-blue-400 flex items-center justify-center ">
+                        <figure className="w-36 h-36 rounded-full bg-brand-blue-light flex items-center justify-center ">
                             <Brain size="70px" color="white" />
                         </figure>
                         <p className="text-center text-lg font-medium"  >Learn and Discover</p>
                     </section>
 
                     <section className="space-y-4 w-max h-max">
-                        <figure className="w-36 h-36 rounded-full bg-red-400 flex items-center justify-center ">
+                        <figure className="w-36 h-36w-36 h-36 rounded-full bg-brand-gray-dark flex items-center justify-center ">
                             <Play size="70px" color="white" />
                         </figure>
                         <p className="text-center text-lg font-medium">Instant feedback</p>
                     </section>
                 </div>
 
-                <button onClick={() => handleStart()} className="rounded-full text-neutral-50 cursor-pointer text-lg hover:brightness-110 transition-all bg-blue-700 py-4 px-10">
+                <button onClick={() => handleStart()} className="rounded-full text-neutral-50 font-semibold cursor-pointer text-lg hover:brightness-110 transition-all bg-brand-blue-dark-two py-4 px-16 mx-auto">
                     Start Quiz
                 </button>
             </div>
